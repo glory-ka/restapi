7cc03b6b9931505c60e5c155402a9e120733d84f
glory-ka <57584382+glory-ka@users.noreply.github.com>
2022-06-06 23:38:37 +0000

    Mon, Jun 6, 2022, 11:38 PM +00:00

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 var serverRouter = require('./routes/survey.js');
 
 var app = express();
@@ -15,8 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/orangutan/survey', serverRouter);
 
 
