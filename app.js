b3f2a57faad234753f366f2c3ab460df1ef7c1e8
glory-ka <57584382+glory-ka@users.noreply.github.com>
2022-06-06 23:35:09 +0000

    Mon, Jun 6, 2022, 11:35 PM +00:00

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/orangutan/survey', serverRouter)
+app.use('/orangutan/survey', serverRouter);
 
 
 module.exports = app;
