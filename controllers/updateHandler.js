04dfb96a27849293bc26c1bdb972e0c324a57d91
glory-ka <57584382+glory-ka@users.noreply.github.com>
2022-06-07 17:42:13 +0000

    Tue, Jun 7, 2022, 5:42 PM +00:00

--- controllers/updateHandler.js
+++ controllers/updateHandler.js
@@ -13,6 +13,12 @@ const changeSurveyStatus = ( req, res, next ) => {
             function( error, survey ){
                 if ( error ) return next( error );
 
+                if ( survey == null ){
+                    const err = new Error( 'Survey not found' );
+                    err.status = 404;
+                    return next( err );
+                }
+
                 survery.changeStatus = req.body.status;
         } );
 };
