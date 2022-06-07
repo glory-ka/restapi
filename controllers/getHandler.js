f64918a342bd58588f1e858c4c011759a5aa1e48
glory-ka <57584382+glory-ka@users.noreply.github.com>
2022-06-07 18:33:15 +0000

    Tue, Jun 7, 2022, 6:33 PM +00:00

--- controllers/getHandler.js
+++ controllers/getHandler.js
@@ -1,59 +1,76 @@
 const Survey = require( '../models/surveyModel' );
 const Response = require( '../models/responseModel' );
 const UserInfo = require( '../models/userIdModel' );
+const returnError = require( './errorHandling' );
 
 /** GET CONTROLLER */
 
-const listPublishedSurvey = (req, res, next) => {
-    Survey.find( { status: 'published', ownerName: { $not: { req.userID }} } ) // CHANGE USERID TO USER NAME
-        .exec( {
+const listPublishedSurvey = ( req, res, next ) => {
+
+    await Survey.find( { status: 'published', ownerName: { $not: { req.userID } } } ) // CHANGE USERID TO USER NAME
+        .exec(
             function ( error, survey_list ){
                 if ( error ) return next( error );
 
+                if ( survey_list == null )
+                    returnError ( 'No Survey found', next );
+
                 res.send( JSON.stringify( survey_list.map( survey => survey.surveyName ) ) );
             }
-        } );
+         );
 };
 
 const listAllOpenSurvey = ( req, res, next ) => {
-    Survey.find( { date_close: { $gte: Date() }})
-        .exec( {
+
+    await Survey.find( { date_close: { $gte: Date() } } )
+        .exec(
             function( error, survey_list ){
                 if ( error ) return next( error );
 
+                if ( survey_list == null )
+                    returnError ( 'No Survey found', next );
+
                 res.send( JSON.stringify( survey_list.map( survey => survey.surveyName ) ) );
             }
-        } );
+         );
 };
 
 const surveyDetail = ( req, res, next ) => {
-    Survey.find( { surveyName: req.name } )
-        .exec( {
+
+    await Survey.find( { surveyName: req.name } )
+        .exec(
             function( error, survey ){
                 if ( error ) return next( error );
 
+                if ( survey == null )
+                    returnError( 'Survey not found' , next );
+
                 res.send( JSON.stringify( survey ));
             }
-        } );
+         );
 };
 
 const surveyResponseCount = ( req, res, next ) => {
-    Survey.find( { surveyName: req.name } )
-        .exec( {
+
+    await Survey.find( { surveyName: req.name } )
+        .exec(
             function( error, survey ){
                 if( error ) return next( error );
 
-                Response.find( { servey: servey } )
+                if ( survey == null )
+                    returnError( 'Survey not found', next );
+
+                await Response.find( { servey: servey } )
                     .populate( 'survey' )
-                    .exec( {
+                    .exec(
                         function( error, survey ){
                             if( error ) return next ( error );
 
                             res.send( JSON.stringify( servey.length ) );
                         };
-                    } );
+                     );
             }
-        } );
+         );
 };
 
 export {
