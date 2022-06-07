753ca050a6cf7bdbd6f6a8eedcaae409cdde8258
glory-ka <57584382+glory-ka@users.noreply.github.com>
2022-06-07 04:18:41 +0000

    Tue, Jun 7, 2022, 4:18 AM +00:00

--- controllers/getHandler.js
+++ controllers/getHandler.js
@@ -1,18 +1,18 @@
-const Survey = require('../models/surveyModel');
-const Response = require('../models/responseModel');
-
+const Survey = require( '../models/surveyModel' );
+const Response = require( '../models/responseModel' );
+const UserInfo = require( '../models/userIdModel' );
 
 /** GET CONTROLLER */
 
 const listPublishedSurvey = (req, res, next) => {
     Survey.find( { status: 'published', ownerName: { $not: { req.userID }} } ) // CHANGE USERID TO USER NAME
-        .exec({
+        .exec( {
             function ( error, survey_list ){
                 if ( error ) return next( error );
 
-                res.send( JSON.stringify( survey_list.map( survey => survey.surveyName )));
+                res.send( JSON.stringify( survey_list.map( survey => survey.surveyName ) ) );
             }
-        });
+        } );
 };
 
 const listAllOpenSurvey = ( req, res, next ) => {
@@ -21,9 +21,9 @@ const listAllOpenSurvey = ( req, res, next ) => {
             function( error, survey_list ){
                 if ( error ) return next( error );
 
-                res.send( JSON.stringify( survey_list.map( survey => survey.surveyName )));
+                res.send( JSON.stringify( survey_list.map( survey => survey.surveyName ) ) );
             }
-        });
+        } );
 };
 
 const surveyDetail = ( req, res, next ) => {
