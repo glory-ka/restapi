6373c16144093b200787d70814242f5a584af634
glory-ka <57584382+glory-ka@users.noreply.github.com>
2022-06-07 07:30:35 +0000

    Tue, Jun 7, 2022, 7:30 AM +00:00

--- controllers/putHandler.js
+++ controllers/putHandler.js
@@ -1,9 +1,21 @@
-
+const Survey = require( '../models/surveyModel' );
+const Response = require( '../models/responseModel' );
+const UserInfo = require( '../models/userIdModel' );
 
 /** PUT ROUTE */
 
 const deleteUserResponse = ( req, res, next ) => {
-    console.log("Not yet implemented");
+
+    const validateUser = await UserInfo.find( { userUUID: req.userID } )
+                                    .exec();
+    const validateSurvey = await Survey.find( { surveyName: req.name } )
+                                    .exec();
+
+if ( validateUser == null || validateSurvey == null )
+req.send( JSON.stringify( { Error: "Incorrect user id or survey name" } ) );
+
+if ( ! validateSurvey.isAnswerExist( req.body.reponse ) )
+req.send( JSON.stringify( { Error: "Response doesn't exist" } ) );
 };
 
 const deleteSurvey = ( req, res, next ) => {
