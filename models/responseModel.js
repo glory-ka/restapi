19f6de8736f5585e71bacd5196e94b052fc4a7ee
glory-ka <57584382+glory-ka@users.noreply.github.com>
2022-06-07 02:54:21 +0000

    Tue, Jun 7, 2022, 2:54 AM +00:00

--- models/responseModel.js
+++ models/responseModel.js
@@ -5,6 +5,6 @@ const Schema = mongoose.Schema;
 const responseSChema = new Schema({
     name: {type: String, required: true},
     response: {type: String, required: true},
-    survey: {type: Schema.Types.ObjectId, ref: 'surveryFormat', required: true}
+    survey: {type: Schema.Types.ObjectId, ref: 'surveyFormat', required: true}
 });
 
