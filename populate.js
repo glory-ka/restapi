5c5148b37ad6399742d8a16c1a56a52d1bd1bad5
glory-ka <57584382+glory-ka@users.noreply.github.com>
2022-06-08 01:11:15 +0000

    Wed, Jun 8, 2022, 1:11 AM +00:00

--- populate.js
+++ populate.js
@@ -4,3 +4,13 @@ var UserId = require( './models/userIdModel' );
 
 var mongoose = require( 'mongoose' );
 
+
+var createUser = ( firstName, lastName, uuid ) => {
+
+    const user = new UserId({
+        firstName: firstName,
+        lastName: lastName,
+        userId: uuid
+    });
+
+};
