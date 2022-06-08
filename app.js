b9c148370913722735da605414ea5664b94f5123
glory-ka <57584382+glory-ka@users.noreply.github.com>
2022-06-08 01:03:24 +0000

    Wed, Jun 8, 2022, 1:03 AM +00:00

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require( 'express' );
+var path = require( 'path' );
+var cookieParser = require( 'cookie-parser' );
+var logger = require( 'morgan');
+var catchError = require( 'http-errors' );
 var mongoose = require( 'mongoose' );
 
-var serverRouter = require('./routes/survey.js');
+
+var serverRouter = require( './routes/survey.js' );
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use( logger( 'dev' ) );
+app.use( express.json() );
+app.use( express.urlencoded({ extended: false } ) );
+app.use( cookieParser() );
+app.use( express.static( path.join( __dirname, 'public' ) ) );
 
-app.use('/orangutan/survey', serverRouter);
+app.use( '/orangutan/survey', serverRouter );
+
+app.use( ( req, res, next ) => {
+    next( catchError( 404 ) );
+} );
+
+app.use( () )
 
 module.exports = app;
 
 var mongooseDB = process.env.MONGODB_URL || '-';
-mongoose.connect(mongooseDB, { useNewUrlParse: true, userUnifiedTopology: true })
+mongoose.connect( mongooseDB, { useNewUrlParse: true, userUnifiedTopology: true } )
 
 var connection = mongoose.connection;
 connection.on( 'error' , console.error.bing( console, 'Connection To database Interrupted' ) )
